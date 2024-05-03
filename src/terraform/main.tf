673549b24cb9b6cfba9c96f52fd7675ec18a2762
suresh <surreesh.subramani@hotmail.com>
2024-05-03 19:58:10 +0530

    update

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -11,7 +11,6 @@ terraform {
     storage_account_name = "terraform2575"
     container_name       = "tfstatefile"
     key                  = "Demo.terraform.tfstate"
-    use_oidc             = true
     access_key           = "ARM_ACCESS_KEY_ID"
     subscription_id      = "ARM_SUBSCRIPTION_ID"
     tenant_id            = "ARM_TENANT_ID"
